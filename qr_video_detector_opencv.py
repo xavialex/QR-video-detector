dfded50d803f850108d458a6329db13330ca4cd4
Xavi <xavialex7@gmail.com>
2019-04-11 13:28:38 +0200

    Functions added

--- qr_video_detector_opencv.py
+++ qr_video_detector_opencv.py
@@ -3,27 +3,22 @@ import numpy as np
 import sys
 import time
 
-inputImage = cv2.imread("01.jpg")
-
 # Display barcode and QR code location
-def display(im, bbox):
+def display(img, bbox):
     n = len(bbox)
     for j in range(n):
-        cv2.line(im, tuple(bbox[j][0]), tuple(bbox[(j+1) % n][0]), (255,0,0), 3)
+        cv2.line(img, tuple(bbox[j][0]), tuple(bbox[(j+1) % n][0]), (255,0,0), 3)
  
     # Display results
-    cv2.imshow("Results", im)
+    return img
 
-qrDecoder = cv2.QRCodeDetector()
-
-def detect_qr(img): 
+def detect_qr(img, qrDecoder): 
     # Detect and decode the qrcode
     data, bbox, rectified_image = qrDecoder.detectAndDecode(img)
     if len(data)>0:
         print("Decoded Data : {}".format(data))
         display(img, bbox)
         rectified_image = np.uint8(rectified_image)
-        cv2.imshow("Rectified QRCode", rectified_image)
         return bbox, rectified_image
     else:
         print("QR Code not detected")
@@ -35,12 +30,16 @@ def detect_qr(img):
 
 
 def main():
-    video = cv2.VideoCapture(0)
+    #input_image = cv2.imread("01.jpg")
+    video = cv2.VideoCapture(1)
+    qrDecoder = cv2.QRCodeDetector()
     while video.isOpened():
         _, frame = video.read()
-        bbox, rectified_image = detect_qr(frame)
+        bbox, rectified_image = detect_qr(frame, qrDecoder)
         if bbox is not None:
-            display(frame, bbox)
+            detections_img = display(frame, bbox)
+            cv2.imshow("Results", detections_img)
+            cv2.imshow("Rectified QRCode", rectified_image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
